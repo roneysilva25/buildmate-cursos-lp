02f17766c346b9b9f98ad8c5b6698239a492e378
roneysilva25 <sabiagames2@gmail.com>
2025-07-13 12:54:19 -0300

    feat: antecipation page

---
+++ scripts/timer.js
@@ -0,0 +1,36 @@
+function getTimeLeft(endDate) {
+    const todayInMs = new Date().getTime()
+    const endDateInMs = endDate.getTime()
+    const remaining = endDateInMs - todayInMs
+    const timezone = 3
+
+    const daysLeft = remaining/(1000*60*60*24)
+    const hoursLeft = (daysLeft - Math.floor(daysLeft))*24 + timezone
+    const minutesLeft = (hoursLeft - Math.floor(hoursLeft))*60
+    const secondsLeft = (minutesLeft - Math.floor(minutesLeft))*60
+
+    return {
+        daysLeft: Math.floor(daysLeft),
+        hoursLeft: Math.floor(hoursLeft),
+        minutesLeft: Math.floor(minutesLeft),
+        secondsLeft: Math.floor(secondsLeft)
+    }
+}
+
+function updateTimer() {
+    const DATE_THE_COURSE_WILL_HAPPEN = new Date("2025-07-16")
+    setInterval(() => {
+        const timer = getTimeLeft(DATE_THE_COURSE_WILL_HAPPEN)
+        document.querySelector("#days").innerHTML = timer.daysLeft
+        document.querySelector("#hours").innerHTML = timer.hoursLeft
+        document.querySelector("#minutes").innerHTML = timer.minutesLeft
+        document.querySelector("#seconds").innerHTML = timer.secondsLeft
+
+        if (DATE_THE_COURSE_WILL_HAPPEN.getTime() - new Date().getTime() <= 0) {
+            document.querySelector("#timer").innerHTML = "🎉CURSO EM ANDAMENTO!"
+            clearInterval(1)
+        }
+    }, 1000)
+}
+
+updateTimer()
