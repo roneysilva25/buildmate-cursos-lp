ea6640259e6abeb075ee05b8c24d6a610b6df36c
roneysilva25 <sabiagames2@gmail.com>
2025-07-15 22:32:32 -0300

    feat: footer and CTA section
    finished landing page

--- scripts/index.js
+++ scripts/index.js
@@ -20,7 +20,7 @@ function onClickThumbnail() {
         videoplayerdiv.innerHTML = `
             <iframe
                 class="w-full h-full rounded-2xl"
-                src="https://www.youtube.com/embed/AEku7X83eRg?autoplay=1"
+                src="https://www.youtube.com/embed/I9lJj6GhlYg?autoplay=1"
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
